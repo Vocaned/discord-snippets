78890bb79cc8f500fbaf8f4a3b558bb9ffea3ca3
Vocaned <10358674+Vocaned@users.noreply.github.com>
2023-02-22 22:09:57 +0200

    Clean up printModuleById

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const V = new function(){
             }
         }
     }
+    this.printModuleById = (id) => {
+        for (let c of webpackChunkdiscord_app) if (id in c[1]) return c[1][id].toString();
+    }
 
     this.enableExperiments = () => {
         u = this.findByProps('getUsers');
@@ -17,13 +20,5 @@ const V = new function(){
         u.__proto__.getCurrentUser().flags |= 1;
         m.find((x)=>x.name === "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
         try {m.find((x)=>x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user:{flags: 1}})} catch {}
-    }
-    this.printModuleById = (id) => {
-        for (let chunk in webpackChunkdiscord_app) {
-            if (!(webpackChunkdiscord_app[chunk] instanceof Array)) continue;
-            if (id in webpackChunkdiscord_app[chunk][1]) {
-                return webpackChunkdiscord_app[chunk][1][id].toString();
-            }
-        }
     }
 }()
