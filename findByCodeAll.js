c893470c6bf9ffcf36fe1de227cc90c1d0cb68ae
Fam0r <fam0r@mailbox.org>
2023-06-29 19:12:44 +0300

    Clean up stuff

--- findByCodeAll.js
+++ findByCodeAll.js
@@ -1,3 +1,12 @@
-r = webpackChunkdiscord_app.push([[Symbol()],{},e=>e]);webpackChunkdiscord_app.pop();
+_r = webpackChunkdiscord_app.push([[Symbol()],{},e=>e]);webpackChunkdiscord_app.pop();
 
-findByCodeAll = (code) => Object.entries(r.m).filter(([,m])=>m.toString().match(code)).map(([id])=>r.c[id]?.exports).filter(m=>m);
+findByCodeAll = (...code) => {
+    let f = [];
+    for (let m of _mods) {
+        if (code.every(c=>typeof m.exports==="function"&&m.exports.toString().includes(c))) f.push(m.exports);
+        for (let ex in m.exports) {
+            if (code.every(c=>typeof m.exports?.[ex]==="function"&&m.exports?.[ex].toString().includes(c))) f.push(m.exports[ex]);
+        }
+    }
+    return f;
+}
--- findByPropsAll.js
+++ findByPropsAll.js
@@ -1,16 +1,15 @@
 _mods = webpackChunkdiscord_app.push([[Symbol()],{},({c})=>Object.values(c)]);webpackChunkdiscord_app.pop();
 
 findByPropsAll = (...props) => {
-    let found = [];
+    let f = [];
     for (let m of _mods) {
         try {
-            if (!m.exports || m.exports === window) continue;
-            if (props.every((x) => m.exports?.[x])) found.push(m.exports);
-
+            if (!m.exports||m.exports===window) continue;
+            if (props.every(x=>m.exports?.[x])) f.push(m.exports);
             for (let ex in m.exports) {
-                if (props.every((x) => m.exports?.[ex]?.[x])) found.push(m.exports[ex]);
+                if (props.every(x=>m.exports?.[ex]?.[x])) f.push(m.exports[ex]);
             }
         } catch {}
     }
-    return found;
+    return f;
 }
